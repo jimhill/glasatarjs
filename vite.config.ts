c66f3c44b6b5031ea01c1a65da5101587597cd19
Jim Hill <ukjimbobbery@gmail.com>
2025-08-06 23:05:54 +0100

    fix: use root base path for custom domain deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   root: './demo',
-  base: process.env.NODE_ENV === 'production' ? '/glasatarjs/' : '/',
+  base: '/',
   build: {
     outDir: '../dist-demo',
   },
